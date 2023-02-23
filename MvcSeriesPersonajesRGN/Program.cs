75c9eb1aa56832c5f0fba7090168f534108b1a11
RafaaGarcia <rafa.gn@hotmail.com>
2023-02-23 04:59:13 +0100

    commit

--- MvcSeriesPersonajesRGN/Program.cs
+++ MvcSeriesPersonajesRGN/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Series}/{action=Index}/{id?}");
 
 app.Run();
